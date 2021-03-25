054ab1e6459b153ede10bcee403678ddb47c9d4b
SurabhiTawniya <surabhitawniya@gmail.com>
2021-03-25 16:40:14 +0530

    GetWithoutBDDApproach

---
+++ src/test/java/ReqresTestGet.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class ReqresTestGet {
+
+	@Test
+	
+	public void reqresWithoutBDDApproach() {
+		
+		RequestSpecification request = RestAssured.given();
+		//request.baseUri("http://reqres.in");
+		//request.queryParam("page", 1);
+		request.log();
+		Response response = request.get("/api/users?page=1");
+		
+		String responseString = response.asString();
+		System.out.println("Response Details:" + responseString);
+		
+		
+	}
+}
